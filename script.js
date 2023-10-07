bc040e056105e3d54c1c47a0f0690956df1e131e
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-07 19:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function toCase(text) {
   // write your code here
+	function toCase(text) {
+  // Check if the input text is empty
+  if (text === '') {
+    return '-';
+  }
+
+  // Convert the text to lowercase and uppercase using toLowerCase() and toUpperCase() methods
+  const lowerCaseText = text.toLowerCase();
+  const upperCaseText = text.toUpperCase();
+
+  // Join the lowercase and uppercase texts with "-"
+  const resultText = lowerCaseText + '-' + upperCaseText;
+
+  return resultText;
 }
 
 // DO not change the code below
